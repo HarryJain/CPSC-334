6c47a033b90de1cb5388d699a04d8e0b95613e3f
HarryJain <harryjain99@gmail.com>
2021-09-28 00:58:24 -0400

    Further cleaned up audio files

--- Module2/Task1/music_tests/new_pygame_music.py
+++ Module2/Task1/music_tests/new_pygame_music.py
@@ -1,13 +1,14 @@
 import pygame
 
-pygame.mixer.pre_init(22050, -16, 2, 1024)
-pygame.init()
-pygame.mixer.quit()
-pygame.mixer.init(22050, -16, 2, 1024)
+#pygame.mixer.pre_init(22050, -16, 2, 1024)
+pygame.mixer.pre_init(48000, -16, 2, 4096)
+#pygame.mixer.init(22050, -16, 2, 1024)
 
-pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('eleanor.wav')
+pygame.mixer.music.play()
 
-pygame.mixer.Channel(0).play(pygame.mixer.Sound('taxman.ogg'), -1)
+#pygame.mixer.Channel(0).play(pygame.mixer.Sound('eleanor.wav'), -1)
 
 while True:
     continue
