779e537a270df1c1692b1e9bff7de10d6728a8cf
HarryJain <HarryJain99@gmail.com>
2021-10-28 18:05:02 -0400

    Commented final code

--- Module4-5/Task1/final/final.ino
+++ Module4-5/Task1/final/final.ino
@@ -1,42 +1,60 @@
+// Stepper imports and constants
 #include <Stepper.h>
 
+// Number of steps per 360 degree revolution
 #define STEPSPERREVOLUTION 2048
 
 // ULN2003 Motor Driver Pins
-#define IN1 19
-#define IN2 18
-#define IN3 5
-#define IN4 17
+#define DRIVER_IN1 19
+#define DRIVER_IN2 18
+#define DRIVER_IN3 5
+#define DRIVER_IN4 17
 
+// Speed constant for rpm
 #define SPEED 10
 
-Stepper my_stepper(STEPSPERREVOLUTION, IN1, IN3, IN2, IN4);
+// Stepper object for 28BYJ-48 stepper motor
+Stepper stepper(STEPSPERREVOLUTION, DRIVER_IN1, DRIVER_IN3, DRIVER_IN2, DRIVER_IN4);
 
 
+// Server imports and constants
 #include <Servo.h>
 
-#define SERVO 4
+// Pin for server input
+#define SERVO_PIN 4
 
+// Servo object for MG90S servo motor
 Servo servo;
+
+// Set initial server position to 0
 int servo_pos = 0;
 
 
+// Set up the stepper and server motors
 void setup() {
-  // set the speed at SPEED rpm
-  my_stepper.setSpeed(SPEED);
-  // attach the servo pin
-  servo.attach(SERVO);
+  // Set the stepper speed at SPEED rpm
+  stepper.setSpeed(SPEED);
+  // Attach the servo pin
+  servo.attach(SERVO_PIN);
+  // Reset the stepper position
   servo.write(servo_pos);
-  // initialize the serial port
+  // Initialize the serial port
   Serial.begin(115200);
 }
 
+
+// Move the stepper and servo such that each time the stepper motor rotates
+//  360 degrees, the servo motor rotates 10 degrees until it reaches 180, when
+//  it returns to 0 degrees
 void loop() {
-  // step one revolution in one direction:
-  my_stepper.step(STEPSPERREVOLUTION);
+  // Move the stepper one revolution (360 degrees)
+  stepper.step(STEPSPERREVOLUTION);
+  // Rotate the servo 10 degrees
   servo_pos += 10;
+  // At 180 degrees, return the servo to 0 degrees
   if (servo_pos > 180) {
     servo_pos = 0;
   }
+  // Rotate the servo to the calculated position
   servo.write(servo_pos);
 }
